1cf7a3cdd39a5124899186ef795f486916f67fd7
samsakhare <samsakhare@gmail.com>
2026-08-19 17:02:17 +0530

    Use @prisma/adapter-pg in shared client

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,7 +1,11 @@
-﻿import { PrismaClient } from '@prisma/client';
+﻿import { Pool } from 'pg';
+import { PrismaPg } from '@prisma/adapter-pg';
+import { PrismaClient } from '@prisma/client';
 
 export * from '@prisma/client';
 
-export const prisma = new PrismaClient({
-  log: ['query'],
-});
+const connectionString = process.env.DATABASE_URL;
+
+const pool = new Pool({ connectionString });
+const adapter = new PrismaPg(pool);
+export const prisma = new PrismaClient({ adapter, log: ['query'] });
